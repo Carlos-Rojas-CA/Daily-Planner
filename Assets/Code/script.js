1ddabdcabfd59fe952fd9b4f23283a78e0aea7a7
Carlos <c2rojas75@gmail.com>
2019-10-19 09:58:33 -0700

    1st upload

---
+++ Assets/Code/script.js
@@ -0,0 +1,102 @@
+moment().format()
+var scheduleElement = $('#schedule')
+var currentDayElement = $('#currentDay')
+var schedule9Element = $('#0');
+var schedule10Element = $('#1');
+var schedule11Element = $('#2');
+var schedule12Element = $('#3');
+var schedule1Element = $('#4');
+var schedule2Element = $('#5');
+var schedule3Element = $('#6');
+var schedule4Element = $('#7');
+var schedule5Element = $('#8');
+
+var times = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM']
+
+var myDay = {
+    date: moment().format('L'),
+    time: ["9AM", '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'],
+    appointment: ['', '', '', '', '', '', '', '', ''],
+};
+
+var old = JSON.parse(window.localStorage.getItem("Planner"))
+if(myDay.date === old.date){
+    myDay = old;
+}
+// window.localStorage.setItem("Planner", JSON.stringify(myDay));
+// console.log(window.localStorage.getItem("Planner"))
+// console.log(myDay.date)
+// var test = JSON.parse(window.localStorage.getItem("Planner"))
+// console.log(test.time[0])
+// var ban = 3
+// console.log(scheduleElement.children('#' + ban).children('input'))
+
+currentDayElement.text(moment().format('dddd, MMMM Do YYYY'))
+
+
+function loadApptPPF(){
+    var hour = moment().hour()-9;
+    for (var i = 0; i<myDay.time.length; i++){
+        scheduleElement.children('#' + i).children('input').attr('value', myDay.appointment[i]);
+        
+        if (hour > i){
+            scheduleElement.children('#' + i).children('input').attr('class', 'col-9 event common past')
+        } else if ( hour === i){
+            scheduleElement.children('#' + i).children('input').attr('class', 'col-9 event common present')
+        } else {
+            scheduleElement.children('#' + i).children('input').attr('class', 'col-9 event common future')
+        }
+
+
+    }
+}
+
+
+scheduleElement.on('click', function(event){
+    var element = event.target;
+    if(element.matches("button")===true){
+        event.preventDefault();
+        var index = element.parentElement.getAttribute("id");
+        var thing = scheduleElement.children('#' + index).children('input').val()
+        myDay.appointment[index] = thing;
+        window.localStorage.setItem("Planner", JSON.stringify(myDay));
+        console.log(thing)
+    }
+})
+
+function loadAppointments(){
+
+}
+
+
+
+
+
+loadApptPPF()
+
+// function createBlocks(){
+//     currentDayElement.text(moment().format('dddd, MMMM Do YYYY'))
+//     for(var i = 0; i<8; i++){
+//         var appointmentBlock = $('<li class="container form row">');
+//         var timeBlock = $('<label class="col-2 text-right time common">');
+//         var eventBlock = $('<input class="col-8 event common" type="text" placeholder="hi">');
+//         var saveBlock = $('<button class="btn col-2 text-center save common">');
+//         timeBlock.text(times[i]);
+//         timeBlock.attr('id', 'time')
+//         eventBlock.text(times[i]);
+//         eventBlock.attr('id', 'event')
+//         saveBlock.text(times[i]);
+//         saveBlock.attr('id', 'save')
+
+//         appointmentBlock.append(timeBlock);
+//         appointmentBlock.append(eventBlock);
+//         appointmentBlock.append(saveBlock)
+        
+//         appointmentBlock.attr('id', i)
+//         scheduleElement.append(appointmentBlock);
+//     }
+// }
+
+
+
+// createBlocks();
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Daily-Planner
+# Unit 05 Third-Party APIs Homework: Day Planner
+
+## Application:
+
+This planner should be about to take in single inputs for events of the day. It will take the current time and date and display it at the top with the date. And, the current time will change the colors of the input boxes based on if it is the current hour. Typing into the input box and clicking save will save the input into the local storage. When the website loads up, it should search your local stoarge for any previous save events for that current date. If it is not the same day, it will rewrite all saved events from previous days.
+
+Since the format for everyday is the same, I just added it to the HTML instead of creating each element using jQuerey or JS.
+
+### Commit history:
+
+1st: The first commit with the comment, 1st upload, is the first commit since starting the repo. It has all functioning parts for this assignment.
+
+
+
+
+
+
+#### Website:
+
+https://carlos-rojas-ca.github.io/Daily-Planner/
+
+![alt text](Assets/Image/Planner.png)
